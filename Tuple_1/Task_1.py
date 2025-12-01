f73c06998c74008be7f9b7efe8878a689b45606a
NikShir29 <shirokoryadovnikita@gmail.com>
2025-12-01 13:50:40 +0300

    Update Task_1.py

--- Tuple_1/Task_1.py
+++ Tuple_1/Task_1.py
@@ -5,9 +5,11 @@ products = {
     "Молочка": [("Молоко", 30, 50), ("Сыр", 40, 80), ("Йогурт", 30, 60)],
     "Сладости": [("Конфеты", 60, 100), ("Зефир", 40, 60), ("Шоколад", 30, 140)]
 }
-
 most_expensive_item = None
 most_expensive_item_price = 0
+most_items_product = None
+most_items_count = 0
+all_products_price = 0
 
 for product in products:
     print(f"\n{product}:")
@@ -16,5 +18,11 @@ for product in products:
         if item[2] > most_expensive_item_price:
             most_expensive_item = item[0]
             most_expensive_item_price = item[2]
+        all_products_price += item[1] * item[2]
+    if sum(item[1] for item in products[product]) > most_items_count:
+        most_items_count = sum(item[1] for item in products[product])
+        most_items_product = product
 
-print(f"\nСамый дорогой продукт: {most_expensive_item} ({most_expensive_item_price} руб.)")
+print(f"\nВ магазине товаров на сумму: {all_products_price} руб.")
+print(f"Больше всего товаров в категории: {most_items_product}, количество: {most_items_count}шт.")
+print(f"Самый дорогой продукт: {most_expensive_item} ({most_expensive_item_price} руб.)")
